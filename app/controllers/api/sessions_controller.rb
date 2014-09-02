e6ee96b7737c3f3c4a394613298e8142d6624f6f
liyijie <liyijie825@gmail.com>
2014-09-02 08:45:56 +0800

    skip verify authenticity token

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::SessionsController < Devise::SessionsController
-  skip_before_filter :verify_authenticity_token,
-  :if => Proc.new { |c| c.request.format == 'application/json' }
+  skip_before_filter :verify_authenticity_token
+  # ,:if => Proc.new { |c| c.request.format == 'application/json' }
 
   respond_to :json
 
